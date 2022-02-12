3fd7faedb18225cd575e8f3cfff70874318c3d68
Leo <83018357+TheRealNeZuX@users.noreply.github.com>
2022-02-12 21:33:28 +0300

    Update iostream.py

--- iostream.py
+++ iostream.py
@@ -1,15 +1,15 @@
-class inout:
+class inout(object):
     def __init__(self, s=""):
         self.val=s
     def __lshift__(self, other):
-        if other.val=="\n":
-            print(other.val)
+        if str(other)=="\n":
+            print(other)
         else:
-            print(other.val, end="")
-        return other
+            print(other, end="")
+        return inout(other)
     def __rshift__(self, other):
         other.val=input()
-        return other
+        return inout(other)
     def __str__(self):
         return str(self.val)
     def __bool__(self):
